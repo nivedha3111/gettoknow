987bdd0d80eea3f2baf68c84d51afb65325193aa
nivedha3111 <nivedhakesav@gmail.com>
2021-03-26 12:55:59 +0530

    interact

--- ran.py
+++ ran.py
@@ -13,13 +13,42 @@ Database = client.get_database('tacti')
 table = Database.ran_list
 #aList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"]
 #sampled_list = random.sample(aList, 5)
+def get_list():
+    aList = ["ajay", "bharat", "cathy", "david", "elizabeth", "fabiana", "gayathri", "hari", "ishwarya", "janani", "kesavaraj", "lajay", "mithra", "nivi", "orange", "preethi", "queeny", "reshma", "sneha"]
+    list1 = random.sample(aList, int(len(aList)/2))
+    list2 = [i for i in aList if i not in list1]
+    return list1, list2
+
+def check(prev_list,list1,list2):
+    old_list1, old_list2=[],[]
+    for l in prev_list:
+        old_list1.append(l['person1'])
+        old_list2.append(l['person2'])
+
+    for p1 in list1:
+        new_id= list1.index(p1)
+        if p1 in old_list1:
+            old_id=old_list1.index(p1)
+            old_p=old_list2[old_id]
+            if old_p == list2[new_id]:
+                return 1
+
+
+
+
+
 
 @app.route('/')
 def home():
-    aList = ["ajay", "bharat", "cathy", "david", "elizabeth", "fabiana", "gayathri", "hari", "ishwarya", "janani", "kesavaraj", "lajay", "mithra", "nivi", "orange", "preethi", "queeny", "reshma", "sneha", "t"]
-    list1 = random.sample(aList, int(len(aList)/2))
-    list2 = [i for i in aList if i not in list1]
+    list1, list2= get_list()
+    
+    prev_list = table.find()
     table.remove( { } );
+    repeat=check(prev_list,list1,list2)
+    if repeat == 1 :
+        list1,list2=get_list()
+    #print(prev_list)
+    #table.remove( { } );
     for i in range(len(list1)):
         table.insert_one({"person1":list1[i], "person2":list2[i] })
     meet_l = table.find()
